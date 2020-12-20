434903ab65225650a424459e6a133f1facb798b9
Konrad <szymoniak.konrad@gmail.com>
2020-12-20 18:28:10 +0100

    add solution to first puzzle from day20

---
+++ day20/src/main.rs
@@ -0,0 +1,133 @@
+use std::collections::hash_map::DefaultHasher;
+use std::collections::HashSet;
+use std::fs;
+use std::hash::{Hash, Hasher};
+
+#[derive(Hash)]
+struct Border<'a>(&'a str);
+
+struct Tile<'a>([&'a Border<'a>; 4]);
+
+struct Orientation {
+    top: u64,
+    right: u64,
+    bottom: u64,
+    left: u64,
+}
+
+struct TileOrientations {
+    id: u64,
+    orientations: Vec<Orientation>,
+}
+
+fn generate_orientations(id: u64, tile: &Tile) -> TileOrientations {
+    let calculate_hash = |border: &Border| {
+        let mut hasher = DefaultHasher::new();
+        border.hash(&mut hasher);
+        hasher.finish()
+    };
+
+    let generate_rotations = |tile: &Tile| -> Vec<Orientation> {
+        (0..4)
+            .map(|i| Orientation {
+                top: calculate_hash(&tile.0[i % 4]),
+                right: calculate_hash(&tile.0[(i + 1) % 4]),
+                bottom: calculate_hash(&tile.0[(i + 2) % 4]),
+                left: calculate_hash(&tile.0[(i + 3) % 4]),
+            })
+            .collect()
+    };
+
+    let mut orientations = Vec::new();
+    orientations.append(&mut generate_rotations(tile));
+
+    // flip horizontally
+    let right = tile.0[1].0.chars().rev().collect::<String>();
+    let right = Border(&right);
+    let left = tile.0[3].0.chars().rev().collect::<String>();
+    let left = Border(&left);
+    orientations.append(&mut generate_rotations(&Tile([
+        tile.0[2], &right, tile.0[0], &left,
+    ])));
+
+    // flip vertically
+    let top = tile.0[0].0.chars().rev().collect::<String>();
+    let top = Border(&top);
+    let bottom = tile.0[2].0.chars().rev().collect::<String>();
+    let bottom = Border(&bottom);
+    orientations.append(&mut generate_rotations(&Tile([
+        &top, tile.0[3], &bottom, tile.0[1],
+    ])));
+
+    TileOrientations { id, orientations }
+}
+
+fn part_1(tile_orientations: &Vec<TileOrientations>) -> u64 {
+    let mut result = HashSet::new();
+    for (i, tile_orientation_primary) in tile_orientations.iter().enumerate() {
+        'outer: for corner_candidate in tile_orientation_primary.orientations.iter() {
+            for (j, tile_orientation_secondary) in tile_orientations.iter().enumerate() {
+                if i == j {
+                    continue;
+                }
+
+                if tile_orientation_secondary
+                    .orientations
+                    .iter()
+                    .any(|neighbour_candidate| {
+                        neighbour_candidate.bottom == corner_candidate.top
+                            || neighbour_candidate.right == corner_candidate.left
+                    })
+                {
+                    continue 'outer;
+                }
+            }
+            result.insert(tile_orientation_primary.id);
+        }
+    }
+
+    result.iter().product()
+}
+
+fn main() {
+    let content = fs::read_to_string("input").unwrap();
+
+    let mut orientations = Vec::new();
+    let mut tile_str: Vec<&str> = Vec::new();
+    let mut tile_id = 0;
+    for line in content.lines() {
+        if line.contains("Tile") {
+            tile_id = line
+                .trim_start_matches("Tile ")
+                .trim_end_matches(':')
+                .parse::<u64>()
+                .unwrap();
+        } else if line.is_empty() {
+            tile_str.clear();
+        } else {
+            tile_str.push(line);
+        }
+
+        if tile_str.len() == 10 {
+            let right = tile_str
+                .iter()
+                .map(|&line| line.chars().last().unwrap())
+                .collect::<String>();
+            let left = tile_str
+                .iter()
+                .map(|&line| line.chars().next().unwrap())
+                .collect::<String>();
+            orientations.push(generate_orientations(
+                tile_id,
+                &Tile([
+                    &Border(tile_str[0]),
+                    &Border(&right),
+                    &Border(tile_str[9]),
+                    &Border(&left),
+                ]),
+            ));
+        }
+    }
+
+    assert_eq!(part_1(&orientations), 108_603_771_107_737);
+}
